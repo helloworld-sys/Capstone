2ebdf3c97f4b689f658cf1dc3cb38200a2850e46
Nathan Gordon <nathan.codeplus1@gmail.com>
2024-01-11 22:32:20 -0600

    styled the forms added drop down for state selectand  added form spree to contact us

--- components/views/Contact.js
+++ components/views/Contact.js
@@ -1,15 +1,26 @@
 import html from "html-literal";
-import commercial from "../../assets/img/Commercial.jpg";
 
 export default () => html`
   <section id="contact">
     <h2>Contact Us</h2>
-    <img src=${commercial} alt="Commercial buildings" />
     <p>
       We look forward to the opportunity to discuss your tax sales needs. Feel
       free to reach out to Global International LLC. at [Your Email Address] or
       [Your Phone Number]. Let's embark on a journey together to achieve your
       financial goals through strategic tax sales endeavors!
     </p>
+    <form action="https://formspree.io/f/myyrzkak" method="POST">
+    <input type="text" name="first" placeholder="First Name" required />
+    <input type="text" name="last" placeholder="Last Name" required />
+    <input type="email" name="email" placeholder="Email Address" required />
+    <textarea
+      rows="5"
+      cols="60"
+      name="message"
+      placeholder="Enter Message"
+      required
+    ></textarea>
+    <br><button type="submit">Send Message</button>
   </section>
+
 `;
